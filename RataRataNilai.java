6dfaced864ad015bb2e71a6bf83a5eda5dfef37e
NaufalArif2095 <naufalarif200905@gmail.com>
2024-11-04 16:24:52 +0700

    Add files via upload

---
+++ RataRataNilai.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PBO1;
+
+/*
+ * @author
+ * NAMA    : Naufal Arif Maulana
+ * KELAS   : PBO12
+ * NIM     : 23176026
+ * Deskripsi Program : Program ini berisi program untuk menampilkan 
+ * Rata-rata Nilai
+ */
+
+import java.util.Scanner;
+
+public class RataRataNilai {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan Banyaknya Mahasiswa: ");
+        int jumlahMahasiswa = scanner.nextInt();
+        double totalNilai = 0;
+
+        for (int i = 1; i <= jumlahMahasiswa; i++) {
+            System.out.print("Nilai Mahasiswa ke-" + i + ": ");
+            double nilai = scanner.nextDouble();
+            totalNilai += nilai;
+        }
+
+        double rataRata = totalNilai / jumlahMahasiswa;
+        System.out.println("\nMaka, Rata-rata Nilainya adalah " + rataRata);
+        System.out.println("Developed by: Rizki Adam Kurniawan");
+    }
+}
